dd6e4cab1f2b9c4121b2390dc49afbc21304e338
Tanmay Binaykiya <btanmay@gatech.edu>
2018-11-18 21:25:10 -0500

    data loader

--- data_loader.py
+++ data_loader.py
@@ -7,24 +7,38 @@ from torch.utils.data import DataLoader
 # QuestionAnswerPair = namedtuple('QuestionAnswer', ['question', 'answer', 'paragraphId'])
 
 class SquadDataset(data.Dataset):
-    urls = ['https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json']
+    urls = ['https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json']
     name = 'SquadDataset'
-    paragraphs = './data/paragraphs.pt'
+    paragraphs_path = './data/paragraphs.json'
     question_answer_pairs_path = "./data/qAPairs.json"
     word_to_idx_path = "./data/word_to_idx.json"
     idx_to_word_path = "./data/idx_to_word.json"
 
     def __init__(self):
-        self.questionAnswerPairs = json.load(self.question_answer_pairs_path)
-        self.idx_to_word = json.load(self.word_to_idx_path)
-        self.word_to_idx = json.load(self.idx_to_word_path)
+        self.paragraphs = json.load(open(self.paragraphs_path, "r"))
+        self.questionAnswerPairs = json.load(open(self.question_answer_pairs_path, "r"))
+        self.idx_to_word = json.load(open(self.word_to_idx_path, "r"))
+        self.word_to_idx = json.load(open(self.idx_to_word_path, "r"))
 
     def __len__(self):
         return len(self.questionAnswerPairs)
 
     def __getitem__(self, index):
+        print("GET ITEM: ", self.questionAnswerPairs[index])
         return self.questionAnswerPairs[index]
 
 
-val = SquadDataset()
-print(val.__getitem__(1))
+if __name__ == '__main__':
+    val = SquadDataset()
+    print(val.__getitem__(1))
+
+    train_loader = DataLoader(val, batch_size=1, shuffle=True, num_workers=1)
+
+    for z in next(iter(train_loader)):
+        # z = next(iter())
+        print("z: ", z)
+
+
+class CustomDataLoader(DataLoader):
+
+    pass
