3f8be1c7c209505ca508753c60803b618027f275
pramodith <pballapuram3@gatech.edu.com>
2018-11-21 03:17:26 -0500

    Adding collate function to accomodate batching of variable length questions and answers.

--- data_loader.py
+++ data_loader.py
@@ -1,10 +1,11 @@
 import json
-
+import numpy as np
 from torch.utils import data
 from torch.utils.data import DataLoader
-
+import torch
 
 # QuestionAnswerPair = namedtuple('QuestionAnswer', ['question', 'answer', 'paragraphId'])
+
 
 class SquadDataset(data.Dataset):
     urls = ['https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json']
@@ -23,20 +24,56 @@ class SquadDataset(data.Dataset):
     def __len__(self):
         return len(self.questionAnswerPairs)
 
+
     def __getitem__(self, index):
-        print("GET ITEM: ", self.questionAnswerPairs[index])
+        #print("GET ITEM: ", self.questionAnswerPairs[index])
         return self.questionAnswerPairs[index]
+
+def collate_fn(data):
+    """Creates mini-batch tensors from the list of tuples (src_seq, trg_seq).
+    We should build a custom collate_fn rather than using default collate_fn,
+    because merging sequences (including padding) is not supported in default.
+    Seqeuences are padded to the maximum length of mini-batch sequences (dynamic padding).
+    Args:
+        data: list of tuple (src_seq, trg_seq).
+            - src_seq: torch tensor of shape (?); variable length.
+            - trg_seq: torch tensor of shape (?); variable length.
+    Returns:
+        src_seqs: torch tensor of shape (batch_size, padded_length).
+        src_lengths: list of length (batch_size); valid length for each padded source sequence.
+        trg_seqs: torch tensor of shape (batch_size, padded_length).
+        trg_lengths: list of length (batch_size); valid length for each padded target sequence.
+    """
+
+    def merge(sequences):
+        lengths = [len(seq) for seq in sequences]
+        padded_seqs = torch.zeros(len(sequences), max(lengths)).long()
+        for i, seq in enumerate(sequences):
+            end = lengths[i]
+            padded_seqs[i, :end] = torch.LongTensor(np.asarray(seq[:end]))
+        return padded_seqs, lengths
+
+    # sort a list by sequence length (descending order) to use pack_padded_sequence
+    #data.sort(key=lambda x: len(x[0]), reverse=True)
+    data.sort(key=lambda x: len(x[1]), reverse=True)
+    # seperate source and target sequences
+    src_seqs, trg_seqs,p_id = zip(*data)
+
+    # merge sequences (from tuple of 1D tensor to 2D tensor)
+    src_seqs, src_lengths = merge(src_seqs)
+    trg_seqs, trg_lengths = merge(trg_seqs)
+
+    return src_seqs, src_lengths, trg_seqs, trg_lengths,p_id
 
 
 if __name__ == '__main__':
     val = SquadDataset()
-    print(val.__getitem__(1))
 
-    train_loader = DataLoader(val, batch_size=1, shuffle=True, num_workers=1)
-
-    for z in next(iter(train_loader)):
-        # z = next(iter())
-        print("z: ", z)
+    train_loader = DataLoader(val, batch_size=4, shuffle=True, num_workers=0,collate_fn=collate_fn)
+    iterator =iter(train_loader)
+    for data in train_loader:
+        print(data[0])
+        #print("z: ", z)
 
 
 class CustomDataLoader(DataLoader):
